c7362470747f67fb300933e4547f3c61cde001ca
Lucas <lucashuati@hotmail.com>
2017-08-24 08:17:20 -0300

    lessons 1-23

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search'
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyALYb6MJgfaN7iGTPoFzQI3Z3Kg09Kifxc';
 
+// Create a new component. This component should produce some HTML
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { videos: [] }
+    YTSearch({key: API_KEY, term: 'nostalgia'}, videos => {
+      this.setState({ videos }); //this.setState( videos: videos )
+    });
+  }
+  render() {
+    return(
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+// Take this component's generated HTML and put it on tha page(in the DOM)
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    <App />,
+    document.querySelector('.container')
+  );
